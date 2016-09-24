468d489206d40530cb269331addbafc79be8d20c
no8-nishino <nishino@novel-g.co.jp>
2016-09-24 14:51:14 +0900

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,37 +1,49 @@
+'use strict';
 
-self.addEventListener('push', function (event) {
+self.addEventListener('push', function(event) {
+  console.log('Received a push message', event);
 
-  if (event.data) {
+  // サンプルでは固定のメッセージを通知するようにしています。
+  // 動的にユーザーごとにメッセージを変えたい場合は、
+  // ペイロードの暗号化を行うか、FetchAPIで動的に情報を取得する必要があります。
+  var title = '新着記事のお知らせです';
+  var body = 'ServiceWorkerの記事を公開しました';
+  var icon = 'test.png';
+  var tag = 'simple-push-demo-notification-tag';
+  var url = 'https://no8-nishino.github.io/';
 
-      var postd = event.data.json();
-
-      console.log(postd);
-
-      var notificationTitle = postd.title;
-      var notificationOptions = {
-	  body: postd.body,
-	  icon: 'image/icon.png',
-	  data: {url: postd.url}
-      };
-  }
-
-  event.waitUntil(Promise.all([self.registration.showNotification(notificationTitle, notificationOptions)]));
+  event.waitUntil(
+    self.registration.showNotification(title, {
+      body: body,
+      icon: icon,
+      tag: tag,
+      data: {
+        url: url
+      }
+    })
+  );
 });
 
-
-self.addEventListener('notificationclick', function (event) {
-
-  console.log('kkk');
-
+self.addEventListener('notificationclick', function(event) {
+  console.log('On notification click: ', event.notification.tag);
   event.notification.close();
 
-  var clickResponsePromise = Promise.resolve();
-
-  if (event.notification.data && event.notification.data.url) {
-
-    console.log('jjj');
-
-    clickResponsePromise = clients.openWindow(event.notification.data.url);
-
+ var notoficationURL = "/"
+  if (event.notification.data.url) {
+    notoficationURL = event.notification.data.url
   }
+  
+  event.waitUntil(clients.matchAll({
+    type: 'window'
+  }).then(function(clientList) {
+    for (var i = 0; i < clientList.length; i++) {
+      var client = clientList[i];
+      if (client.url === '/' && 'focus' in client) {
+        return client.focus();
+      }
+    }
+    if (clients.openWindow) {
+      return clients.openWindow(notoficationURL);
+    }
+  }));
 });
